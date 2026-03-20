744e9f8ff515baaa2596e348d2cc2c7cb1088241
vinisaojudas <vinivieiraxbox360@gmail.com>
2026-03-19 21:13:00 -0300

    Add files via upload

---
+++ Exercicio_3.java
@@ -0,0 +1,10 @@
+public class Exercicio_3 {
+    public static void main (String[] args){
+        int nota_1 = 5;
+        int nota_2 = 8;
+        int nota_3 = 6;
+        double divisao = (nota_1 + nota_2 + nota_3) / 3.0;
+
+        System.out.println ("media = " + divisao); 
+    }
+}
